bfe85ab9d23c713354c593793973b5a5a9169f42
Crystal1921 <90130465+Crystal1921@users.noreply.github.com>
2025-12-09 21:16:51 +0800

    修复gui

--- src/main/java/com/github/tartaricacid/netmusic/client/gui/MusicListPlayerMenuScreen.java
+++ src/main/java/com/github/tartaricacid/netmusic/client/gui/MusicListPlayerMenuScreen.java
@@ -1,6 +1,5 @@
 package com.github.tartaricacid.netmusic.client.gui;
 
-import com.github.tartaricacid.netmusic.NetMusic;
 import com.github.tartaricacid.netmusic.inventory.MusicListPlayerMenu;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
@@ -8,10 +7,14 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 import org.anti_ad.mc.ipn.api.IPNIgnore;
+import org.jetbrains.annotations.NotNull;
 
 @IPNIgnore
 public class MusicListPlayerMenuScreen extends AbstractContainerScreen<MusicListPlayerMenu> {
-    private static final ResourceLocation BG = ResourceLocation.fromNamespaceAndPath(NetMusic.MOD_ID, "textures/gui/music_list_player.png");
+    /**
+     * The ResourceLocation containing the chest GUI texture.
+     */
+    private static final ResourceLocation CONTAINER_BACKGROUND = ResourceLocation.withDefaultNamespace("textures/gui/container/generic_54.png");
 
     public MusicListPlayerMenuScreen(MusicListPlayerMenu menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
@@ -19,14 +22,16 @@ public class MusicListPlayerMenuScreen extends AbstractContainerScreen<MusicList
     }
 
     @Override
-    protected void renderBg(GuiGraphics graphics, float partialTicks, int mouseX, int mouseY) {
-        int posX = (this.width - this.imageWidth) / 2;
-        int posY = (this.height - this.imageHeight) / 2;
-        graphics.blit(BG, posX, posY, 0, 0, this.imageWidth, this.imageHeight);
+    protected void renderBg(GuiGraphics guiGraphics, float partialTicks, int mouseX, int mouseY) {
+        int i = (this.width - this.imageWidth) / 2;
+        int j = (this.height - this.imageHeight) / 2;
+        int containerRows = 3;
+        guiGraphics.blit(CONTAINER_BACKGROUND, i, j, 0, 0, this.imageWidth, containerRows * 18 + 17);
+        guiGraphics.blit(CONTAINER_BACKGROUND, i, j + containerRows * 18 + 17, 0, 126, this.imageWidth, 96);
     }
 
     @Override
-    public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
+    public void render(@NotNull GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
         super.render(graphics, mouseX, mouseY, partialTicks);
         renderTooltip(graphics, mouseX, mouseY);
     }
